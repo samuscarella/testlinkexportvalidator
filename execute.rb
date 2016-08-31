5b49058015c30678806fdf645ace58e1cb389b28
samuscarella <stephen.muscarella@kibocommerce.com>
2016-08-31 15:42:48 -0500

    added more instances

--- execute.rb
+++ execute.rb
@@ -1,3 +1,10 @@
 require './TestLinkExportLogs'
 
 TestLinkExportLogs.new.mapTestSuites("ADMIN Responsive")
+TestLinkExportLogs.new.mapTestSuites("B2C Responsive")
+TestLinkExportLogs.new.mapTestSuites("CCOM")
+TestLinkExportLogs.new.mapTestSuites("Depracated/Deleted")
+TestLinkExportLogs.new.mapTestSuites("Integration Services")
+TestLinkExportLogs.new.mapTestSuites("Modules")
+TestLinkExportLogs.new.mapTestSuites("Security")
+
