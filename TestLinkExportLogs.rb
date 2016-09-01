977d73ae8006ff1ab62298bc67bcdbc380a7e641
samuscarella <stephen.muscarella@kibocommerce.com>
2016-09-01 11:12:31 -0500

    Updates

--- TestLinkExportLogs.rb
+++ TestLinkExportLogs.rb
@@ -7,39 +7,38 @@ require 'Nokogiri'
 
     def mapTestSuites(category)
 
-      testSuitesArray = Array.new
-      
-      Dir.chdir "/Users/#{ENV['USER']}/Desktop/TestLinkExportLogs/#{category}" do
-          xmlFiles = Dir["*.xml"]
-          xmlFiles.each do |xml|
-            doc = Nokogiri::XML(File.open(xml))
-            testSuites = doc.xpath("//testsuite")
-            testsuite_data = testSuites.map { |ts|
-              {
-                suitename: ts["name"],
-                testCaseCount: ts.xpath("./testcase").count,
-                testCaseNames: ts.xpath("./testcase").map { |tc|
-                  {
-                    testcaseName: tc["name"]
-                  }
+        testSuitesArray = Array.new
+
+        Dir.chdir "/Users/#{ENV['USER']}/Desktop/TestLinkExportLogs/#{category}" do
+
+            xmlFiles = Dir["*.xml"]
+            xmlFiles.each do |xml|
+              doc = Nokogiri::XML(File.open(xml))
+              testSuites = doc.xpath("//testsuite")
+              testsuite_data = testSuites.map { |ts|
+                {
+                  suiteName: ts["name"],
+                  testCaseCount: ts.xpath("./testcase").count,
                 }
               }
-            }
-            testSuitesArray.push(testsuite_data)
-          end
-      end
-
-        Dir.chdir "/Users/#{ENV['USER']}/Desktop/TestLinkExportLogs"
-      
-          File.open("./../#{category}_results.txt", "w+") { |f|
-            f.write("You're looking at the results of #{category}:\n\n")
-            testSuitesArray.each do |suite|
-                suite.each do |s|
-                  f.write("#{s.dig(:suitename)} = (#{s.dig(:testCaseCount)})\n")
-                end
+              # puts testsuite_data
+              testSuitesArray.push(testsuite_data)
             end
+
+        end
+
+          Dir.chdir "/Users/#{ENV['USER']}/Desktop/TestLinkExportLogs"
+
+            File.open("./../#{category}_results.txt", "w+") { |f|
+
+              f.write("You're looking at the results of #{category}:\n\n")
+              testSuitesArray[0].each do |element|
+                  # puts element.dig(:suitename)
+                  f.write("\t\t#{element.dig(:suiteName)} = #{element.dig(:testCaseCount)}\n")
+              end
+              f.close
+            }
             puts "Logged #{category} successfully..."
-          }
      end
-    
+
   end
--- execute.rb
+++ execute.rb
@@ -1,10 +1,9 @@
 require './TestLinkExportLogs'
 
-TestLinkExportLogs.new.mapTestSuites("ADMIN Responsive")
+# TestLinkExportLogs.new.mapTestSuites("ADMIN Responsive")
 TestLinkExportLogs.new.mapTestSuites("B2C Responsive")
-TestLinkExportLogs.new.mapTestSuites("CCOM")
-TestLinkExportLogs.new.mapTestSuites("Depracated or Deleted")
-TestLinkExportLogs.new.mapTestSuites("Integration Services")
-TestLinkExportLogs.new.mapTestSuites("Modules")
-TestLinkExportLogs.new.mapTestSuites("Security")
-
+# TestLinkExportLogs.new.mapTestSuites("CCOM")
+# TestLinkExportLogs.new.mapTestSuites("Depracated or Deleted")
+# TestLinkExportLogs.new.mapTestSuites("Integration Services")
+# TestLinkExportLogs.new.mapTestSuites("Modules")
+# TestLinkExportLogs.new.mapTestSuites("Security")
